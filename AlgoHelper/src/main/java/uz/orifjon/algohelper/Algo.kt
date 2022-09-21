21f20d3e26f69a6b99ac72e6fcf3139ab80200fb
Orifjon <orifjon.olimjonov@mail.ru>
2022-09-21 09:11:53 +0500

    Add Binary Search

--- AlgoHelper/src/main/java/uz/orifjon/algohelper/Algo.kt
+++ AlgoHelper/src/main/java/uz/orifjon/algohelper/Algo.kt
@@ -8,4 +8,19 @@ object Algo {
         }
         return false
     }
+
+    fun binarySearch(searchNumber:Int,list:ArrayList<Int>):Int{
+        var low = 0
+        var high = list.size-1
+        var mid:Int
+        while(low <= high) {
+            mid = low + ((high - low) / 2)
+            when {
+                searchNumber >list[mid]   -> low = mid+1
+                searchNumber == list[mid] -> return mid
+                searchNumber < list[mid]  -> high = mid-1
+            }
+        }
+        return -1
+    }
 }
